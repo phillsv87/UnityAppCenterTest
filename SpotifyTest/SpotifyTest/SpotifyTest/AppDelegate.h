9bd91fa2d23296979ad50fe35006e6a0882cfa48
Scott Vance <logic@newbaselogic.com>
2020-01-27 10:28:35 +0100

    - Created SpotifyTest project

---
+++ SpotifyTest/SpotifyTest/SpotifyTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpotifyTest
+//
+//  Created by Phillip Vance on 1/26/20.
+//  Copyright © 2020 Phillsv87. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SpotifyTest/SpotifyTest/SpotifyTest/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  SpotifyTest
+//
+//  Created by Phillip Vance on 1/26/20.
+//  Copyright © 2020 Phillsv87. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
